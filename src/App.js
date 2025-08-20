e7f456dc5f22ae416a031ae63f03c7a096506b3d
Saadath Kassim <saadathadunniade@gmail.com>
2025-08-20 11:03:00 +0100

    Merged local project with remote repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
+
+
