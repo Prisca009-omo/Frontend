71a39a45c89f99f755a5a3cc67ca415426ca1ead
gitXena <zina.njoku@gmail.com>
2025-08-26 10:26:45 +0100

    put components in seperate Home Page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { useState } from 'react'
-// import Home from './components/Home'
+// import Home from './Home'
 import "./index.css";
 // import "./global.css";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-// import SingleReview from "./components/SingleReview";
-import TestimonialCarousel from "./components/TestimonialCarousel";
-import Service from "./components/Services";
-import AboutUs01 from "./components/AboutUs01";
-import WorkingProcess from "./components/WorkingProcess";
-import AboutUs02 from "./components/AboutUs02";
-import Portfolio from "./components/Portfolio";
-import ClientLogo from "./components/ClientLogo";
-import CTA from "./components/CTA";
-import Reviews from './components/Reviews'
-import ProfesionalsTeam from "./components/ProfesionalsTeam";
-import Pricing from "./components/Pricing";
-import ContactForm from "./components/ContactForm";
-import BlogSection from "./components/BlogSection";
-import LogoSection from "./components/LogoSection";
+import Navbar from "./components/Home Page/Navbar";
+import Hero from "./components/Home Page/Hero";
+// import SingleReview from "./SingleReview";
+import TestimonialCarousel from "./components/Home Page/TestimonialCarousel";
+import Service from "./components/Home Page/Services";
+import AboutUs01 from "./components/Home Page/AboutUs01";
+import WorkingProcess from "./components/Home Page/WorkingProcess";
+import AboutUs02 from "./components/Home Page/AboutUs02";
+import Portfolio from "./components/Home Page/Portfolio";
+import ClientLogo from "./components/Home Page/ClientLogo";
+import CTA from "./components/Home Page/CTA";
+import Reviews from './components/Home Page/Reviews'
+import ProfesionalsTeam from "./components/Home Page/ProfesionalsTeam";
+import Pricing from "./components/Home Page/Pricing";
+import ContactForm from "./components/Home Page/ContactForm";
+import BlogSection from "./components/Home Page/BlogSection";
+import LogoSection from "./components/Home Page/LogoSection";
 
 function App() {
 
