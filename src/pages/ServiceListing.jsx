8a38b73bac1b3572d450693633b8cf46abcc478c
gitXena <zina.njoku@gmail.com>
2025-09-18 15:02:49 +0100

    fixed navbar not sticking in mobile

--- src/pages/ServiceListing.jsx
+++ src/pages/ServiceListing.jsx
@@ -4,8 +4,10 @@ import ListedServices from "../components/Service Page/ListedServices";
 export default function ServicesListings() {
   const [services, setServices] = useState([]);
 
+  const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
   useEffect(() => {
-    fetch("http://localhost:3000/service/services")
+    fetch(`${backendUrl}/service/services`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
